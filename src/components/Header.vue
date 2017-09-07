18790194d3c3d939746211f87b393acf034b2b35
Kyle-Koivukangas <kyle.koivukangas@gmail.com>
2017-09-07 11:06:03 -0700

    Navbar set up need to work on routing.

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,17 +1,63 @@
 <template>
-    <div class="component-container">
-        <h1>Header</h1>
+    <div class="navbar">
+        <router-link to="/" tag="button" class="nav-btn" active-class="active" exact>Home</router-link>
+        <router-link v-for="(link, linkName) in links" :key="linkName" :to="link" class="nav-btn" active-class="active">
+            {{ linkName }}
+        </router-link>
     </div>
 </template>
 
 <script>
 export default {
+    data() {
+        return {
+            links: {
+                'Stocks': '/stocks',
+                'Portfolio': '/portfolio'
+            }
+        }
+    }
 
 }
 </script>
 
 <style lang="scss" scoped>
-    @import '../styles/_variables.scss';
+@import '../styles/_variables.scss';
 
+.active {
+    background-color: $highlight;
+    color: $lightFontColor;
+}
 
+.navbar {
+    height: 50px;
+    overflow: hidden;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-family: $font1;
+    font-weight: 500;
+    color: black;
+    background-color: $mid;
+}
+
+.nav-btn {
+    padding: 5px 5px 5px 5px;
+    background-color: $mid;
+    font-family: $font2;
+    color: black;
+    cursor: pointer;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    &:focus {
+        // outline: 0;
+        background-color: $highlight;
+    }
+    &:active {
+        background-color: $darkerMid;
+    }
+}
 </style>
