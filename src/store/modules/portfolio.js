75887370b13a0aeda261aea897354a8c44ddac21
Kyle-Koivukangas <kyle.koivukangas@gmail.com>
2017-09-08 14:11:43 -0700

    Store set up

--- src/store/modules/portfolio.js
+++ src/store/modules/portfolio.js
@@ -1,15 +1,51 @@
 //portfolio state store
 const state = {
-
+    funds: 10000,
+    ownedStocks: [],
+    transactions: [],
 }
 const getters = {
-
+    ownedStocks: state => {
+        return state.ownedStocks
+    },
+    transactions: state => {
+        return state.transactions
+    },
+    funds: state => {
+        return state.funds
+    }
 }
 const mutations = {
-
+    'BUY_STOCK'(state, { stockTicker, quantity, stockPrice, date }) {
+        const record = state.ownedStocks.find(element => element.stockTicker == stockTicker);
+        console.log(`record: ${record}`);
+        if (record) {
+            record.quantity += quantity;
+        } else {
+            state.ownedStocks.push({
+                stockTicker: stockTicker,
+                quantity: quantity,
+            })
+        }
+        state.funds -= stockPrice * quantity;
+        state.transactions.push({ order: 'BUY', stockTicker, quantity, stockPrice, date });
+    },
+    'SELL_STOCK'(state, { stockTicker, quantity, stockPrice, date }) {
+        const record = state.ownedStocks.find(element => element.stockTicker == stockTicker);
+        if (record.quantity >= quantity) {
+            record.quantity -= quantity;
+        } else {
+            console.log(`not enough stock to sell that many ${record}`)
+            console.log(record);
+        }
+        state.funds += stockPrice * quantity;
+        state.transactions.push({ order: 'SELL', stockTicker, quantity, stockPrice, date });
+    },
 }
 const actions = {
-
+    sellStock: ({ commit }, order) => {
+        commit('SELL_STOCK', order)
+    },
 }
 
 
