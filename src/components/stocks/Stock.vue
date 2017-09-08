6ec45d670c2e329b530b0499cea270763414e82c
Kyle-Koivukangas <kyle.koivukangas@gmail.com>
2017-09-07 17:14:38 -0700

    Stocks Page built built the stocks page, may need some visual tweaks. Still needs some logic changes for the change arrow symbol and it's just using test info for now, need to set up store

--- src/components/stocks/Stock.vue
+++ src/components/stocks/Stock.vue
@@ -1,17 +1,65 @@
 <template>
-    <div class="component-container">
-        <h3>Stock component</h3>
+    <div class="component-container stock-component">
+        <div class="stock-header">
+            <h3 class="text-center">{{ stock.name }}</h3>
+        </div>
+        <div class="info">
+            <ul>
+                <li>Ticker: {{ stock.ticker }}</li>
+                <li>Price: {{ stock.price }}</li>
+                <li>Change: {{ exampleInfo.change }} <i class="fa fa-arrow-down" aria-hidden="true"> </i></li>
+            </ul>
+        </div>
+
     </div>
 </template>
 
 <script>
 export default {
+    props: ['stock'],
+    data() {
+        return {
+            exampleInfo: {
+                ticker: "TICKER",
+                price: "$50.54",
+                change: "Down .50"
+            },
+            stockChange: "up 0.54",
+        }
 
+    },
 }
 </script>
 
 <style lang="scss" scoped>
-    @import '../../styles/_variables.scss';
+@import '../../styles/_variables.scss';
 
+.stock-component {
+    background-color: white;
+    margin: 10px;
+    @include border-radius(5px);
+    border: 1.2px solid $dark;
+    padding-top: 0px;
+}
 
+.stock-header {
+    background-color: $transDark;
+    vertical-align: top;
+    margin-top: 0px;
+}
+.info {
+    font-family: $font2;
+}
+
+h3 {
+    font-family: $font2;
+    font-weight: 400;
+    margin-top: 0px;
+}
+ul {
+    list-style-type: none;
+}
+li i {
+    color: red;
+}
 </style>
--- src/components/stocks/StocksDisplay.vue
+++ src/components/stocks/StocksDisplay.vue
@@ -1,17 +1,58 @@
 <template>
     <div class="component-container">
-        <h2>Stocks Display Page</h2>
+        <h2 class="text-center">Stock Information</h2>
+        <hr style="width: 90%;">
+        <div class="stocks-grid">
+            <app-stock v-for="stock in stocks" :key="stock" :stock="stock"></app-stock>
+        </div>
     </div>
 </template>
 
 <script>
-export default {
+import { mapGetters } from 'vuex';
 
+export default {
+    data() {
+        return {
+            stocksList: ['Google', 'IBM', 'Apple', 'HP', 'company1', 'company2', 'company3', 'company4'],
+            stocks: [
+                { id: 1, name: 'Tesla', ticker: 'HP', price: 249.64 },
+                { id: 2, name: 'Google', ticker: 'GOOGL', price: 111.97 },
+                { id: 3, name: 'IBM', ticker: 'AAPL', price: 83.04 },
+                { id: 4, name: 'Apple', ticker: 'IBM', price: 95.06 },
+                { id: 5, name: 'HP', ticker: 'HP', price: 65.78 },
+            ]
+        }
+    },
+    components: {
+        appStock: () => import('./Stock.vue'),
+    },
+    // computed: {
+    //     ...mapGetters(['stocksList']),
+    //     getStocks() {
+    //         return this.$store.state.stocksList;
+    //     }
+    // }
 }
 </script>
 
 <style lang="scss" scoped>
-    @import '../../styles/_variables.scss';
+@import '../../styles/_variables.scss';
 
+.stocks-grid {
+    display: grid;
+    @include mq('phone-wide') {
+        grid-template: repeat(2, 1fr) / repeat(2, 1fr);
+    }
+    @include mq('tablet') {
+        grid-template: repeat(3, 1fr) / repeat(3, 1fr);
+    }
+    @include mq('desktop-wide') {
+        grid-template: repeat(4, 1fr) / repeat(4, 1fr);        
+    }
+}
 
+h2 {
+    font-family: $font1;
+}
 </style>
