0a3a515df258eb672180a01c88f12e11f06328b3
Kyle-Koivukangas <kyle.koivukangas@gmail.com>
2017-09-09 15:29:26 -0700

    Added picture to homepage

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -7,7 +7,12 @@
 
         <div class="content">
             <br>
-            <p>Welcome to Stock Trader! Your online stock marketplace.</p>
+            <div class="img-container"><img class="img1" src="../assets/matplotlib.png" alt=""></div>
+            <h4>Welcome to Stock Trader! Your online <br> stockmarket - market-marketingplace!!</h4>
+            
+            <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae nisi iure, dolor, accusantium natus dignissimos earum laborum asperiores nesciunt id explicabo sunt aliquam repellat inventore, vitae placeat excepturi sint reprehenderit. </p>
+
+            <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae nisi iure, dolor, accusantium natus dignissimos earum laborum asperiores nesciunt id explicabo sunt aliquam repellat inventore, vitae placeat excepturi sint reprehenderit. </p>
             <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae nisi iure, dolor, accusantium natus dignissimos earum laborum asperiores nesciunt id explicabo sunt aliquam repellat inventore, vitae placeat excepturi sint reprehenderit. </p>
             <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae nisi iure, dolor, accusantium natus dignissimos earum laborum asperiores nesciunt id explicabo sunt aliquam repellat inventore, vitae placeat excepturi sint reprehenderit. </p>
             <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae nisi iure, dolor, accusantium natus dignissimos earum laborum asperiores nesciunt id explicabo sunt aliquam repellat inventore, vitae placeat excepturi sint reprehenderit. </p>
@@ -32,11 +37,20 @@ export default {
 <style lang="scss">
 @import '../styles/_variables.scss';
 
+.img-container {
+    float:right;
+}
+.img1 {
+    height: 200px;
+    width: 300px;
+}
+
 .title-box {
     @include center--auto();
     font-family: $font1;
     width: 90%;
 }
+
 .content {
     @include center--auto();
     width: 90%;
